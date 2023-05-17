e4ac1001165ff4a8a21022d0de15458f06e57a1a
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2023-05-17 23:41:04 +0900

    refact: 移动 unity init 脚本

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,69 @@
 import { createApp } from 'vue';
 import './styles.scss';
+// @ts-ignore
 import App from './App.vue';
+import isMobile from 'ismobilejs';
+
+const container = document.querySelector("#unity-container") as HTMLDivElement;
+const canvas = document.querySelector("#unity-canvas") as HTMLCanvasElement;
+const loadingBar = document.querySelector("#unity-loading-bar") as HTMLDivElement;
+const progressBarFull = document.querySelector("#unity-progress-bar-full") as HTMLDivElement;
+const fullscreenButton = document.querySelector("#unity-fullscreen-button") as HTMLButtonElement;
+
+const buildUrl = "/Build";
+const loaderUrl = buildUrl + "/ShirokoAdventrue.loader.js";
+const config = {
+  dataUrl: buildUrl + "/ShirokoAdventrue.data.unityweb",
+  frameworkUrl: buildUrl + "/ShirokoAdventrue.framework.js.unityweb",
+  codeUrl: buildUrl + "/ShirokoAdventrue.wasm.unityweb",
+  streamingAssetsUrl: "StreamingAssets",
+  companyName: "CopperArchive",
+  productName: "ShirokoAdventureInKivotos",
+  productVersion: "0.1",
+};
+
+// By default Unity keeps WebGL canvas render target size matched with
+// the DOM size of the canvas element (scaled by window.devicePixelRatio)
+// Set this to false if you want to decouple this synchronization from
+// happening inside the engine, and you would instead like to size up
+// the canvas DOM size and WebGL render target sizes yourself.
+// config.matchWebGLToCanvasSize = false;
+
+if (isMobile(navigator).any) {
+  // Mobile device style: fill the whole browser client area with the game canvas:
+
+  const meta = document.createElement('meta');
+  meta.name = 'viewport';
+  meta.content = 'initial-scale=1.0, user-scalable=no';
+  document.getElementsByTagName('head')[0].appendChild(meta);
+  container.className = "unity-mobile";
+  canvas.className = "unity-mobile";
+
+  // To lower canvas resolution on mobile devices to gain some
+  // performance, uncomment the following line:
+  // config.devicePixelRatio = 1;
+} else {
+  // Desktop style: Render the game canvas in a window that can be maximized to fullscreen:
+  canvas.style.width = "1280px";
+  canvas.style.height = "720px";
+}
+
+loadingBar.style.display = "block";
+
+const script = document.createElement("script");
+script.src = loaderUrl;
+script.onload = () => {
+  createUnityInstance(canvas, config, (progress: number) => {
+    progressBarFull.style.width = 100 * progress + "%";
+  }).then((unityInstance) => {
+    loadingBar.style.display = "none";
+    fullscreenButton.onclick = () => {
+      unityInstance.SetFullscreen(1);
+    };
+  }).catch((message) => {
+    alert(message);
+  });
+};
+document.body.appendChild(script);
 
 createApp(App).mount('#app');
