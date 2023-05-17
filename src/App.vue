a97044c9e310334368452cfc4b9a7a0b840fc034
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2023-05-18 01:03:35 +0900

    chore: 移动端适配预备

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed, ref } from "vue";
+
+const isLandscape = ref(true);
+
+const { orientation } = window.screen;
+orientation.onchange = () => {
+  isLandscape.value = orientation.type.includes("landscape");
+};
+</script>
 
 <template>
-  <div></div>
+  <Teleport to="body" v-if="!isLandscape">
+    <div class="test"></div>
+  </Teleport>
 </template>
 
 <style scoped></style>
