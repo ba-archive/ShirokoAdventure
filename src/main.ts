b1a15307cf0c652d03fd64feb863e26787ff89f6
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2023-05-20 12:47:07 +0900

    style: 优化加载提示

--- src/main.ts
+++ src/main.ts
@@ -4,23 +4,31 @@ import './styles.scss';
 import App from './App.vue';
 import isMobile from 'ismobilejs';
 
-const container = document.querySelector("#unity-container") as HTMLDivElement;
-const canvas = document.querySelector("#unity-canvas") as HTMLCanvasElement;
-const loadingBar = document.querySelector("#unity-loading-bar") as HTMLDivElement;
-const progressBarFull = document.querySelector("#unity-progress-bar-full") as HTMLDivElement;
-const fullscreenButton = document.querySelector("#unity-fullscreen-button") as HTMLDivElement;
-const loadingPercentage = document.querySelector("#loading-percentage") as HTMLDivElement;
+const container = document.querySelector('#unity-container') as HTMLDivElement;
+const canvas = document.querySelector('#unity-canvas') as HTMLCanvasElement;
+const loadingBar = document.querySelector(
+  '#unity-loading-bar'
+) as HTMLDivElement;
+const progressBarFull = document.querySelector(
+  '#unity-progress-bar-full'
+) as HTMLDivElement;
+const fullscreenButton = document.querySelector(
+  '#unity-fullscreen-button'
+) as HTMLDivElement;
+const loadingPercentage = document.querySelector(
+  '#loading-percentage'
+) as HTMLDivElement;
 
-const buildUrl = "/Build";
-const loaderUrl = buildUrl + "/ShirokoAdventrue.loader.js";
+const buildUrl = '/Build';
+const loaderUrl = buildUrl + '/ShirokoAdventrue.loader.js';
 const config = {
-  dataUrl: buildUrl + "/ShirokoAdventrue.data.unityweb",
-  frameworkUrl: buildUrl + "/ShirokoAdventrue.framework.js.unityweb",
-  codeUrl: buildUrl + "/ShirokoAdventrue.wasm.unityweb",
-  streamingAssetsUrl: "StreamingAssets",
-  companyName: "CopperArchive",
-  productName: "ShirokoAdventureInKivotos",
-  productVersion: "0.1",
+  dataUrl: buildUrl + '/ShirokoAdventrue.data.unityweb',
+  frameworkUrl: buildUrl + '/ShirokoAdventrue.framework.js.unityweb',
+  codeUrl: buildUrl + '/ShirokoAdventrue.wasm.unityweb',
+  streamingAssetsUrl: 'StreamingAssets',
+  companyName: 'CopperArchive',
+  productName: 'ShirokoAdventureInKivotos',
+  productVersion: '0.1',
 };
 
 // By default Unity keeps WebGL canvas render target size matched with
@@ -37,9 +45,9 @@ if (isMobile(navigator).any) {
   meta.name = 'viewport';
   meta.content = 'initial-scale=1.0, user-scalable=no';
   document.getElementsByTagName('head')[0].appendChild(meta);
-  container.className = "unity-mobile";
-  canvas.className = "unity-mobile";
-  fullscreenButton.className = "unity-mobile";
+  container.className = 'unity-mobile';
+  canvas.className = 'unity-mobile';
+  fullscreenButton.className = 'unity-mobile';
 
   // To lower canvas resolution on mobile devices to gain some
   // performance, uncomment the following line:
@@ -50,28 +58,28 @@ if (isMobile(navigator).any) {
   // canvas.style.height = "720px";
 }
 
-loadingBar.style.display = "block";
-
-const script = document.createElement("script");
+const script = document.createElement('script');
 script.src = loaderUrl;
 script.onload = () => {
   createUnityInstance(canvas, config, (progress: number) => {
-    progressBarFull.style.width = 100 * progress + "%";
+    progressBarFull.style.width = 100 * progress + '%';
     if (progress - 0.89 <= 0) {
-      loadingPercentage.innerText = (progress * 100).toFixed(2) + "%";
+      loadingPercentage.innerText = (progress * 100).toFixed(2) + '%';
     } else {
-      loadingPercentage.innerText = "正在解析游戏资源……";
+      loadingPercentage.innerText = '正在解析游戏资源……';
+      loadingPercentage.classList.add('no-after');
     }
-  }).then((unityInstance) => {
-    loadingBar.style.display = "none";
-    loadingPercentage.style.display = "none";
-    window.unityInstance = unityInstance;
-    fullscreenButton.onclick = () => {
-      unityInstance.SetFullscreen(1);
-    };
-  }).catch((message) => {
-    alert(message);
-  });
+  })
+    .then(unityInstance => {
+      loadingBar.style.display = "none";
+      window.unityInstance = unityInstance;
+      fullscreenButton.onclick = () => {
+        unityInstance.SetFullscreen(1);
+      };
+    })
+    .catch(message => {
+      alert(message);
+    });
 };
 document.body.appendChild(script);
 
